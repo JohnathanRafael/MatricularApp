f890a021e7a78bf839bba2c1c359b80949fbb909
Johnathan Rafael <nahtanhoj.555@gmail.com>
2024-04-29 20:00:49 -0300

    Route Fly OK

--- lib/app/app_page.dart
+++ lib/app/app_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:routefly/routefly.dart';
+import '../routes.dart';
 import 'login/login_page.dart';
 
 void main() {
@@ -12,21 +14,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: Routefly.routerConfig(
+        routes: routes, // GENERATED
+        initialPath: routePaths.path,
+        routeBuilder: (context, settings, child) {
+          return MaterialPageRoute(
+            settings: settings, // !! IMPORTANT !!
+            builder: (context) => child,
+          );
+        },
+      ),
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const Login(title: 'Matricular'),
+      //home: const Login(title: 'Matricular'),
     );
   }
 }
 
 class Login extends StatefulWidget {
   const Login({super.key, required this.title});
-
 
   final String title;
 
@@ -42,7 +53,7 @@ class _LoginState extends State<Login> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Matricular'),
       ),
       body: const LoginForm(),
     );
